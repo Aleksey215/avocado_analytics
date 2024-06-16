60bb71e499c7015dc4833aea3113cf30d8cbdf79
Aleksey <kaloha215@gmail.com>
2024-06-16 20:19:22 +1000

    create layout and run app

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ Main app file (initialization app)
 import pandas as pd
 from dash import Dash, dcc, html
 
+# prepare data for using in app
 data = (
     pd.read_csv("avocado.csv")
     .query("type== 'conventional' and region == 'Albany'")
@@ -11,4 +12,47 @@ data = (
     .sort_values(by="Date")
 )
 
+# create instance of Dash (init app)
 app = Dash(__name__)
+
+app.layout = html.Div(
+    children=[
+        html.H1(
+            children="Avocado Analitics",
+            style={"fontSize": "48px", "color": "red"},
+        ),
+        html.P(
+            children=(
+                "Analyze the behavior of avocado prices and the number"
+                " of avocados sold in the US between 2015 and 2018"
+            ),
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["AveragePrice"],
+                        "type": "lines",
+                    },
+                ],
+                "layout": {"title": "Average Price of Avocados"},
+            },
+        ),
+        dcc.Graph(
+            figure={
+                "data": [
+                    {
+                        "x": data["Date"],
+                        "y": data["Total Volume"],
+                        "type": "Lines",
+                    },
+                ],
+                "layout": {"title": "Avocados Sold"},
+            },
+        ),
+    ]
+)
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
