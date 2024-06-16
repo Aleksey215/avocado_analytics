2f6d9cba620b8ce99a3c88dcfcb26811b403eb50
Aleksey <kaloha215@gmail.com>
2024-06-16 21:04:37 +1000

    add external style

--- app.py
+++ app.py
@@ -11,22 +11,35 @@ data = (
     .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
     .sort_values(by="Date")
 )
-
+external_stylesheets = [
+    {
+        "href": (
+            "https://fonts.googleapis.com/css2?"
+            "family=Lato:wght@400;700&display=swap"
+        ),
+        "rel": "stylesheet",
+    },
+]
 # create instance of Dash (init app)
-app = Dash(__name__)
-
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Avocado Analytics: Understand Your Avocados!"
 app.layout = html.Div(
     children=[
-        html.H1(
-            children="Avocado Analitics",
-            # style={"fontSize": "48px", "color": "red"},
-            className="header-title",
-        ),
-        html.P(
-            children=(
-                "Analyze the behavior of avocado prices and the number"
-                " of avocados sold in the US between 2015 and 2018"
-            ),
+        html.Div(
+            children=[
+                html.P(children="🥑", className="header-emoji"),
+                html.H1(
+                    children="Avocado Analytics", className="header-title"
+                ),
+                html.P(
+                    children=(
+                        "Analyze the behavior of avocado prices and the number"
+                        " of avocados sold in the US between 2015 and 2018"
+                    ),
+                    className="header-description",
+                ),
+            ],
+            className="header",
         ),
         dcc.Graph(
             figure={
