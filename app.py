47dc4ce104e907d5ac563f8b6df27b8bbcbedb4c
Aleksey <kaloha215@gmail.com>
2024-06-16 16:39:25 +1000

    add app initialization

---
+++ app.py
@@ -0,0 +1,14 @@
+"""
+Main app file (initialization app)
+"""
+import pandas as pd
+from dash import Dash, dcc, html
+
+data = (
+    pd.read_csv("avocado.csv")
+    .query("type== 'conventional' and region == 'Albany'")
+    .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
+    .sort_values(by="Date")
+)
+
+app = Dash(__name__)
