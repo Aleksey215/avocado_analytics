78e67a7cf2912369ed4f51d65352196aee4e62a5
Aleksey <kaloha215@gmail.com>
2024-06-23 14:06:35 +1000

    add dropdowns

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ from dash import Dash, dcc, html
 # prepare data for using in app
 data = (
     pd.read_csv("avocado.csv")
-    .query("type== 'conventional' and region == 'Albany'")
     .assign(Date=lambda data: pd.to_datetime(data["Date"], format="%Y-%m-%d"))
     .sort_values(by="Date")
 )
+regions = data["region"].sort_values().unique()
+avocado_types = data["type"].sort_values().unique()
 external_stylesheets = [
     {
         "href": (
@@ -42,6 +43,59 @@ app.layout = html.Div(
                 ),
             ],
             className="header",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Region", className="manu-title"),
+                        dcc.Dropdown(
+                            id="region-filter",
+                            options=[
+                                {"label": region, "value": region}
+                                for region in regions
+                            ],
+                            value="Albany",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+                html.Div(
+                    children=[
+                        html.Div(children="Type", className="manu-title"),
+                        dcc.Dropdown(
+                            id="type-filter",
+                            options=[
+                                {
+                                    "label": avocado_type.title(),
+                                    "value": avocado_type,
+                                }
+                                for avocado_type in avocado_types
+                            ],
+                            value="organic",
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Date Range", className="menu-title"
+                        ),
+                        dcc.DatePickerRange(
+                            id="date-range",
+                            min_date_allowed=data["Date"].min().date(),
+                            max_date_allowed=data["Date"].max().date(),
+                            start_date=data["Date"].min().date(),
+                            end_date=data["Date"].max().date(),
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
         ),
         html.Div(
             children=[
